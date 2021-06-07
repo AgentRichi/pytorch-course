0c74ee95c0ea6df8982df526b29c095637fbd22c
Richard Rossmann <rossmann.richard@gmail.com>
2021-06-07 16:47:32 +0000

    CNN section complete

--- docker_run.sh
+++ docker_run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker run --gpus all --rm -p 8888:8888 -e JUPYTER_ENABLE_LAB=yes -v /media/richard/New\ Volume:/home/jovyan/work:rw jupyter/pytorch-course
+sudo docker run --gpus all --rm -p 8888:8888 -e JUPYTER_ENABLE_LAB=yes -v /media/richard/:/home/jovyan/work:rw jupyter/pytorch-course
